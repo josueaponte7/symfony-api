b86ff0a07962c1710b5cf3f72d4c552415ea1c42
Josue Aponte <josueaponte7@gmail.com>
2022-03-14 22:33:54 +0100

    Create Form

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -4,11 +4,12 @@ namespace App\Controller\Api;
 
 
 use App\Entity\Book;
+use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class BooksController extends AbstractFOSRestController
 {
@@ -26,12 +27,16 @@ class BooksController extends AbstractFOSRestController
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function create(EntityManagerInterface $em)
+    public function create(EntityManagerInterface $em, Request $request)
     {
         $book = new Book();
-        $book->setTitle('Mas Libro');
-        $em->persist($book);
-        $em->flush();
-        return $book;
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($book);
+            $em->flush();
+            return $book;
+        }
+        return $form;
     }
 }
